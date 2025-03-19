9f7538adc02c918b673d97e971ad2e410fb3a426
Aakash Gupta <aakashguptaa23@gmail.com>
2025-03-19 22:49:52 +0530

    Removed unused variables

--- src/components/InvitePage.tsx
+++ src/components/InvitePage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { AUTH_REGISTER, API_INVITE_SCORE } from "../constants";
+import { AUTH_REGISTER } from "../constants";
 import "../styles/InvitePage.css";
 
 const InvitePage: React.FC = () => {
