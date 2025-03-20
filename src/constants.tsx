c83552706a4bae2f4b3f2adf8aa4a726eafc827e
Acash512 <aakashguptaa23@gmail.com>
2025-03-20 05:31:34 +0530

    fix the env

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,9 +1,11 @@
 export const API_BASE_URL =
-  import.meta.env.REACT_APP_API_URL || "https://globetrotter-backend-three.vercel.app";
+  import.meta.env.VITE_API_URL ||
+  "https://globetrotter-backend-three.vercel.app";
 export const AUTH_REGISTER = `${API_BASE_URL}/auth/register`;
 export const AUTH_LOGIN = `${API_BASE_URL}/auth/login`;
 export const FRONTEND_URL =
-  import.meta.env.FRONTEND_URL || "https://globetrotter-frontend-kappa.vercel.app";
+  import.meta.env.VITE_FRONTEND_URL ||
+  "https://globetrotter-frontend-kappa.vercel.app";
 export const API_RANDOM_LOCATION = `${API_BASE_URL}/locations/random`;
 export const API_SUBMIT_ANSWER = `${API_BASE_URL}/locations/submit`;
 export const API_INVITE_SCORE = `${API_BASE_URL}/locations/invite-score`;
